d2b42f41a6ec9d6281d55e7732f1338f36451cd0
annicoletti <andre.nicoletti@live.com>
2020-02-18 13:03:22 -0300

    SDK

---
+++ src/test/java/br/com/cpqd/avm/sdk/test/Sample.java
@@ -0,0 +1,27 @@
+package br.com.cpqd.avm.sdk.test;
+
+import java.util.Scanner;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+public class Sample {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String json = null;
+
+		json = sc.next();
+
+		if (json != null) {
+			Object object = new JSONTokener(json).nextValue();
+			if (object instanceof JSONObject) {
+				JSONObject jsonObject = new JSONObject(json);
+				System.out.println(jsonObject.getBoolean("status"));
+			} else {
+				System.out.println("não é json");
+			}
+		}
+
+	}
+}
